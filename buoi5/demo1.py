a296e142f9ec88cfc876a67621c5ef12ad5b3eef
FME849 <1414439@gmail.com>
2023-10-26 19:24:30 +0700

    buoi5: demo1

---
+++ buoi5/demo1.py
@@ -0,0 +1,11 @@
+#input text:str
+text:str = input("Please input a string: ")
+
+#output output:str
+output:str = ''
+
+#process
+for i in range(len(text) - 1, -1, -1):
+    output += text[i]
+
+print(output)
