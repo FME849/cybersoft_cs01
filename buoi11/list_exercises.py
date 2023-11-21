0eaa1456ecc4c5afa2e3338692e7eef06f88ce7c
FME849 <1414439@gmail.com>
2023-11-21 20:17:40 +0700

    update: def find_index()

--- buoi11/list_exercises.py
+++ buoi11/list_exercises.py
@@ -5,3 +5,4 @@ lst_number = [18, 9, 4, 4, 2, 15, 16, 19, 27, 44, 50, 15, 15]
 # Ex 1: Viết hàm tìm ra số lớn nhất trong list
 
 print(method.find_max(lst_number))
+print(method.find_indexes(lst_number))
--- buoi11/method.py
+++ buoi11/method.py
@@ -7,3 +7,19 @@ def find_max(lst: list[int]):
         if n > max_number:
             max_number = n
     return max_number
+
+def find_indexes(lst: list[int]):
+    # input: user_input:int
+    user_input:int = int(input("Please input a number: "))
+
+    # output:
+    output:list[int] = []
+
+    # process
+    for idx in range(0, len(lst)):
+        if lst[idx] == user_input:
+            output.append(idx)
+    if len(output) > 0:
+        return (f"Các vị trí chứa {user_input} là: {output}")
+    else:
+        return ("Không tìm thấy giá trị")
