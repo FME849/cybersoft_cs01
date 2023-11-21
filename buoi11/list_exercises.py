140f318819e503458274c3253f12afdce9b6e7e1
FME849 <1414439@gmail.com>
2023-11-21 21:00:24 +0700

    update: def del_item()

--- buoi11/list_exercises.py
+++ buoi11/list_exercises.py
@@ -6,3 +6,4 @@ lst_number = [18, 9, 4, 4, 2, 15, 16, 19, 27, 44, 50, 15, 15]
 
 print(method.find_max(lst_number))
 print(method.find_indexes(lst_number))
+print(method.delete_item(lst_number))
--- buoi11/method.py
+++ buoi11/method.py
@@ -23,3 +23,19 @@ def find_indexes(lst: list[int]):
         return (f"Các vị trí chứa {user_input} là: {output}")
     else:
         return ("Không tìm thấy giá trị")
+
+def delete_item(lst: list[int]):
+        # input: user_input:int
+    user_input:int = int(input("Please input a number: "))
+
+    # output:str
+    output:str = "Không tìm thấy giá trị"
+
+    #process
+    for i in range(len(lst) - 1, -1, -1):
+        if lst[i] == user_input:
+            del lst[i];
+            output = f"list sau khi xoa cac phan tu {user_input}: {lst}"
+
+        
+    return output
