2222bb5aaf55489ba3686ccb3333c9341e58689f
FME849 <1414439@gmail.com>
2023-12-14 19:49:18 +0700

    buoi17: todo_list

---
+++ buoi17/to_do_list.py
@@ -0,0 +1,44 @@
+def add_task():
+    task_name = input("Input task name: ")
+    task = {
+        "task_name": task_name,
+        "status": task_status[0],
+    }
+    task_list.append(task)
+
+def display_task_list():
+    for task in task_list:
+        print(f'Task name: {task["task_name"]}')
+        print(f'Task status: {task["status"]}')
+        print('------------------')
+
+def check_task():
+    display_task_list()
+    task_name = input("Input task name: ")
+    for task in task_list:
+        if (task["task_name"] == task_name):
+            task["status"] = task_status[1]
+    display_task_list()
+
+running = True
+print('--- To do app ---')
+task_list = []
+task_status = ('Pending', 'Done')
+while running:
+    print('''
+          1/ Add task
+          2/ Display task
+          3/ Check task
+          4/ Exit
+    ''')
+    select:int = int(input("Select function: "))
+    if select == 1:
+        add_task()        
+        print("Successfully add task")
+    elif select == 2:
+        print('--- Task list ---')
+        display_task_list()
+    elif select == 3:
+        check_task()
+    elif (select == 4):
+        running = False
